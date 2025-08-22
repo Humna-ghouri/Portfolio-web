c917e92b3e4af8e0727a0a25cff162544559ac44
Humna-ghouri <monababu001@gmail.com>
2025-08-23 01:42:53 +0500

    work

--- portfolio-new/src/components/Resume/ResumeNew.js
+++ portfolio-new/src/components/Resume/ResumeNew.js
@@ -10,7 +10,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-// ✅ Worker configuration
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
